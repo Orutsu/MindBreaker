3e85815480e5cc59bb2dd2dd54905e11ec399fa5
Dmytro Kozak <nikroshet@gmail.com>
2021-06-12 16:07:21 +0300

    Usage example of db

--- src/screens/Tree/MainTreeScreen/index.tsx
+++ src/screens/Tree/MainTreeScreen/index.tsx
@@ -1,15 +1,29 @@
 import React, { useEffect } from 'react';
 // Components
-import { SafeAreaView, Text, TextInput } from 'react-native';
+import { SafeAreaView, Text, TextInput, Button } from 'react-native';
 import Header from '../../../components/Header';
+
+import { insertFolder, selectFolderById } from '../../../database/actions/foldersTree'
 
 const TreeMainScreen = () => {
   return (
     <SafeAreaView>
       <Header title="Tree" />
-      <Text style={{marginTop: 20}}>
-      TreeMainScreen
+      <Text style={{ marginTop: 20 }}>
+        TreeMainScreen
       </Text>
+      <Button title="suka" onPress={ async() => {
+
+        let id : number;
+        await insertFolder("FolderName", null).then((insertedId) => {
+          id = insertedId;
+        })
+
+        await selectFolderById(id).then((folder) => {
+          console.log(folder)
+        })
+
+      }} />
     </SafeAreaView>
   );
 };
